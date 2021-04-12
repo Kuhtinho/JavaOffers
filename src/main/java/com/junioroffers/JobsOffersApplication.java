3d347bd4e5f8d959c527d26f0e5fbcc585e519e7
Nikita Kuhta <nikitakyxta@gmail.com>
2021-04-12 18:55:29 +0200

    project structure

---
+++ src/main/java/com/junioroffers/JobsOffersApplication.java
@@ -0,0 +1,12 @@
+package com.junioroffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsOffersApplication.class);
+    }
+}
