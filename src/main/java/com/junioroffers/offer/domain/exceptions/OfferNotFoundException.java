b05329a2d8ad21cda5bf8d59d900b6e5f91e6c4c
Mikita <nikitakyxta@gmail.com>
2023-02-26 11:20:42 +0100

    implemented database

--- src/main/java/com/junioroffers/offer/domain/exceptions/OfferNotFoundException.java
+++ src/main/java/com/junioroffers/offer/domain/exceptions/OfferNotFoundException.java
@@ -9,7 +9,7 @@ public class OfferNotFoundException extends RuntimeException{
     private final String offerId;
 
     public OfferNotFoundException(String offerId){
-        super(String.format("Offer with id %d not found", offerId));
+        super(String.format("Offer with id %s not found", offerId));
         this.offerId = offerId;
     }
 
